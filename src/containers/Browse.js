23aef0a82c4227401e538befebb7a15dfec2c239
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-07-05 05:19:45 -0500

    Browse Component queryString Update
    • Created an onNavFiltersChangeHandler to create a string with the search query
    - string is then set to searchQuery state
    
    • created a useEffect that will update the path whenever there is a change to searchQuery  state
    - if first render it pushes to the history
    - else it replaces the history

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 // Import Custom Components
 import NavFilters from '../components/Navs/NavFilters'
 import NavTags from '../components/Navs/NavTags'
@@ -5,10 +7,35 @@ import NavTags from '../components/Navs/NavTags'
 import classes from './Browse.module.scss'
 
 const Browse = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [isFirst, setisFirst] = useState(true)
+  const history = useHistory()
+
+  useEffect(() => {
+    if (isFirst && searchQuery) {
+      setisFirst(false)
+      history.push({ pathname: '/Search', search: searchQuery })
+    }
+
+    !isFirst && history.replace({ pathname: '/Search', search: searchQuery })
+  }, [history, searchQuery, isFirst])
+
+  const onNavFiltersChangeHandler = useCallback((values) => {
+    let queryObj = {}
+
+    for (const [key, value] of Object.entries(values)) {
+      queryObj = { ...queryObj, [key]: value.map((value) => `${key}=${value}`).join('&') }
+    }
+
+    const { search, genres, year, season, format, status, yearRange, episodeRange, durationRange } = { ...queryObj }
+    const queryString = [search, genres, year, season, format, status, yearRange, episodeRange, durationRange].filter((value) => value).join('&')
+    setSearchQuery(queryString ? `?${queryString}` : '')
+  }, [])
+
   return (
     <main className={classes['container']} style={{ paddingTop: '6em', margin: '0 2.5%' }}>
-      <NavFilters />
-      <NavTags />
+      <NavFilters onChange={onNavFiltersChangeHandler} />
+      {/* <NavTags /> */}
     </main>
   )
 }
