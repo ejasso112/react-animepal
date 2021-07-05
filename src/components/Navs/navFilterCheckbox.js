786e7669e3cdb893a1f242ba1de92525e2024d3f
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-07-05 04:28:29 -0500

    navFilterCheckbox Update
    • navFilterCheckbox is now functional
    - added heading props for checkbox name (default: '')
    - added onChange prop that passes value argument to parent component
    - added enabled prop for default checkbox state (default: false)
    - added timeout prop for delay to run onChange method (default: 0)
    - added some styling for hove and active styles
    - (all props are optional)

--- src/components/Navs/navFilterCheckbox.js
+++ src/components/Navs/navFilterCheckbox.js
@@ -1,16 +1,55 @@
+// Import React Dependancies
+import { useState, useEffect, memo } from 'react'
+// Import Custom Hook
+import { useIsMount } from '../../services/customHooks'
 // Import Styles
 import classes from './navFilterCheckbox.module.scss'
 
-const NavFilterCheckbox = (props) => {
-  const { heading } = { ...props }
+//* Nav Checkbox Filter Component
+const NavFilterCheckbox = (
+  props = {
+    heading: '', // ----------------- heading of the Search Filter
+    enabled: false, // -------------- bool to show if checkbox is enabled or not
+    timeout: NaN, // ---------------- timeout time for less rerenders
+    onChange: (isEnabled) => {}, // - function to perform when enabeld toggles
+  }
+) => {
+  // Destructuring Props
+  const { heading, enabled, timeout, onChange } = { ...props }
+  // State to check if Checkbox is enabled or disabled
+  const [isEnabled, setIsEnabled] = useState(enabled || false)
+  // Costum Hook to check is isMount or Rerender
+  const isMount = useIsMount()
+
+  // Effect to run timeout to only run onChange function every 400ms
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      !isMount && onChange && onChange(isEnabled)
+    }, timeout || 0)
+
+    return () => {
+      clearTimeout(identifier)
+    }
+    // eslint-disable-next-line
+  }, [enabled, timeout, onChange])
+
+  // Handler for when Checkbox onClick to toggle isEnabled
+  const onCheckboxClickHandler = () => {
+    setIsEnabled((prevState) => !prevState)
+  }
+
+  const isEnabledClass = isEnabled ? classes['content__box__inner--active'] : classes['content__box__inner--hover']
+  //* Render Nav Checkbox Filter
   return (
     <div className={classes['container']}>
       <div className={classes['content']}>
-        <div className={classes['content__box']}></div>
+        <div className={classes['content__box']} onClick={onCheckboxClickHandler}>
+          <div className={`${classes['content__box__inner']} ${isEnabledClass}`} />
+        </div>
         <div className={classes['content__heading']}>{heading}</div>
       </div>
     </div>
   )
 }
 
-export default NavFilterCheckbox
+export default memo(NavFilterCheckbox)
