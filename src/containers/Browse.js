fed5c2304637eb58c8d358a955d74e9a1f46a4e5
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-07-06 09:55:55 -0500

    Browse Component history push change update
    • Change the way history is being pushed to path
    - Remove all state variables and instead directly push to histoy onChange trigger
    - When there is no search query in history it is pushed to history
    - else it is replaced

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,36 +1,30 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
+
 // Import Custom Components
 import NavFilters from '../components/Navs/NavFilters'
-import NavTags from '../components/Navs/NavTags'
+// import NavTags from '../components/Navs/NavTags'
 
 import classes from './Browse.module.scss'
 
 const Browse = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [isFirst, setisFirst] = useState(true)
   const history = useHistory()
 
-  useEffect(() => {
-    if (isFirst && searchQuery) {
-      setisFirst(false)
-      history.push({ pathname: '/Search', search: searchQuery })
-    }
+  const onNavFiltersChangeHandler = useCallback(
+    (values) => {
+      let queryObj = {}
+      for (const [key, value] of Object.entries(values)) {
+        queryObj = { ...queryObj, [key]: value.map((value) => `${key}=${value}`).join('&') }
+      }
 
-    !isFirst && history.replace({ pathname: '/Search', search: searchQuery })
-  }, [history, searchQuery, isFirst])
+      const { search, genres, year, season, format, status, yearRange, episodeRange, durationRange, hentai } = { ...queryObj }
+      const queryString = [search, genres, year, season, format, status, yearRange, episodeRange, durationRange, hentai].filter((value) => value).join('&')
 
-  const onNavFiltersChangeHandler = useCallback((values) => {
-    let queryObj = {}
-
-    for (const [key, value] of Object.entries(values)) {
-      queryObj = { ...queryObj, [key]: value.map((value) => `${key}=${value}`).join('&') }
-    }
-
-    const { search, genres, year, season, format, status, yearRange, episodeRange, durationRange, hentai } = { ...queryObj }
-    const queryString = [search, genres, year, season, format, status, yearRange, episodeRange, durationRange, hentai].filter((value) => value).join('&')
-    setSearchQuery(queryString ? `?${queryString}` : '')
-  }, [])
+      if (!history.location.search) history.push({ pathname: '/Search', search: queryString })
+      else history.replace({ pathname: '/Search', search: queryString })
+    },
+    [history]
+  )
 
   return (
     <main className={classes['container']} style={{ paddingTop: '6em', margin: '0 2.5%' }}>
